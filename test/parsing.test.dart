d4ff9636149ab0dd6b70785a9769bc3bd11322b9
Philip Purwoko <philippurwoko123@gmail.com>
2021-09-10 22:32:52 +0700

    Fix parsing testing with mock data

--- test/parsing.test.dart
+++ test/parsing.test.dart
@@ -1,58 +1,50 @@
-import 'dart:math';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rankedresto/model/resto_detail_model.dart';
 import 'package:rankedresto/model/resto_list_model.dart';
 import 'package:rankedresto/model/searched_resto.dart';
-import 'package:rankedresto/provider/detail_provider.dart';
-import 'package:rankedresto/provider/list_provider.dart';
+import 'data.mock.dart';
 
 void main() {
-  group('Parsing JSON from API', () {
-    test('Fetch restaurant list data', () async {
-      final ListProvider listProvider = ListProvider();
-      await listProvider.fetchRestaurants();
-      expect(listProvider.restaurants.isNotEmpty, true);
-    });
+  test('Parsing Restaurant List', () {
+    final RestoList restoList = RestoList.fromJson(getJsonOf(DataString.list));
+    expect(restoList.error, false);
+    expect(restoList.message, 'success');
+    expect(restoList.count, 20);
+    expect(restoList.restaurants, hasLength(2));
+    expect(
+      restoList.restaurants.map((Restaurant r) => r.id),
+      contains('rqdv5juczeskfw1e867'),
+    );
+  });
 
-    test('Fetch single detail restaurant', () async {
-      const String id = 'rqdv5juczeskfw1e867';
-      final DetailProvider detailProvider = DetailProvider();
-      await detailProvider.fetchRestaurantById(id);
-      final RestaurantDetail? restaurantDetail =
-          detailProvider.getFetchedRestaurantById(id);
-      expect(restaurantDetail != null, true);
-      expect(restaurantDetail!.id, id);
-      expect(restaurantDetail.name, 'Melting Pot');
-      expect(restaurantDetail.city, 'Medan');
-    });
+  test('Parsing Detail Restaurant', () {
+    final RestoDetail restoDetail =
+        RestoDetail.fromJson(getJsonOf(DataString.detail));
+    expect(restoDetail.error, false);
+    expect(restoDetail.message, 'success');
+    expect(restoDetail.restaurant.id, 'rqdv5juczeskfw1e867');
+    expect(restoDetail.restaurant.name, 'Melting Pot');
+  });
 
-    test('Search restaurant', () async {
-      const String keyword = 'Makan mudah';
-      final List<Restaurant> restaurants = await searchRestaurant(keyword);
-      expect(restaurants.isNotEmpty, true);
-      expect(restaurants.map((Restaurant r) => r.name).contains(keyword), true);
-    });
+  test('Parsing Searching Restaurant', () {
+    final SearchedRestaurant searchedRestaurant =
+        SearchedRestaurant.fromJson(getJsonOf(DataString.search));
+    expect(searchedRestaurant.error, false);
+    expect(searchedRestaurant.founded, 1);
+    expect(searchedRestaurant.restaurants, hasLength(1));
+    expect(searchedRestaurant.restaurants.first.id, 'fnfn8mytkpmkfw1e867');
+  });
 
-    test('Send review', () async {
-      const String id = 'rqdv5juczeskfw1e867';
-      final String randomUniqueName =
-          'test user ${Random().nextInt(1000)} ${DateTime.now().millisecond}';
-      final DetailProvider detailProvider = DetailProvider();
-      await detailProvider.fetchRestaurantById(id);
-      final List<CustomerReview> customerReviews =
-          await detailProvider.sendReview(
-        id: id,
-        name: randomUniqueName,
-        review: 'test review',
-      );
-
-      expect(customerReviews.isNotEmpty, true);
-      expect(
-        customerReviews
-            .map((CustomerReview review) => review.name)
-            .contains(randomUniqueName),
-        true,
-      );
-    });
+  test('Parsing Sending Review Response', () {
+    final CustomerReviewResponse customerReviewResponse =
+        CustomerReviewResponse.fromJson(getJsonOf(DataString.addReview));
+    expect(customerReviewResponse.error, false);
+    expect(customerReviewResponse.message, 'success');
+    expect(customerReviewResponse.customerReviews.isNotEmpty, true);
+    expect(customerReviewResponse.customerReviews, hasLength(2));
+    expect(
+      customerReviewResponse.customerReviews.map((CustomerReview r) => r.name),
+      contains('Ahmad'),
+    );
   });
 }
